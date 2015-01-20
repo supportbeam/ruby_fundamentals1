1890248ecd98fc1a6ee081205fde9c460ff6f266
JJLim <lim.jess9@gmail.com>
2015-01-20 13:03:15 -0500

    Completed exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+# A good tip for a $55 meal
+puts 55 * 0.20
+
+# Adding a string and integer
+puts "Hi there" + 2.to_s
+
+#Using string interpolation
+result =45628 * 7839
+puts "The answer is #{result}."
+
+#Value is true
+puts (true && false) || (false && true) || !(false && false)
